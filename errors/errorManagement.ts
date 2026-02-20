a31a4f61f89c9711f9cdf1d55cd5fbec5bba709c
Yoga Raditya <radityanalaa@gmail.com>
2026-02-21 03:29:20 +0700

    feat: add new error category

--- errors/errorManagement.ts
+++ errors/errorManagement.ts
@@ -2,6 +2,7 @@ const errorManagement = {
     commonErrors: {
         InvalidInput: { message: 'invalid input', status: 400 },
         Unauthorized: { message: 'unauthorized', status: 401 },
+        Forbidden: { message: 'forbidden', status: 403 },
         NotFound: { message: 'not found', status: 404 },
         Conflict: { message: 'conflict', status: 409 },
         InternalServerError: { message: 'internal server error', status: 500 },
