e08913cb77779051c4f9b5b213381ea5b649960b
Athernatus <nizarmw1@gmail.com>
2026-06-12 18:47:12 +0700

    changing the ip server

--- libraries/socket/socket.ts
+++ libraries/socket/socket.ts
@@ -5,7 +5,7 @@ let io: Server;
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "http://44.222.124.225:3000",
+  "http://44.223.39.155:3000",
   "https://groupchat-kabw.akbarfikri.my.id",
   ...(process.env.APP_ORIGIN ? process.env.APP_ORIGIN.split(",").map((origin) => origin.trim()) : []),
 ];
